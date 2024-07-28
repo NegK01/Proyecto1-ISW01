de6abce519c961064435c63f938761db6daa3ba2
Neg <mauricio.or99@gmail.com>
2024-07-28 17:06:21 -0600

    Prints del Main eliminados

--- src/negocio/Main.java
+++ src/negocio/Main.java
@@ -1,6 +1,5 @@
 package negocio;
 
-import dataBase.*;
 import com.formdev.flatlaf.FlatDarculaLaf;
 import interfaz.JFInicioSesión;
 import javax.swing.UIManager;
@@ -18,41 +17,6 @@ public class Main {
         
         JFInicioSesión inicio = new JFInicioSesión();
         inicio.setVisible(true);
-        
-        
-        
-        
-        System.out.println("----------Aerolineas----------");
-        AerolineaClass asd = new AerolineaClass();
-        
-        asd.leerAerolineaTxt();
-        
-        System.out.println(asd.getID() + "\n\n" + asd.getNOMBRE());
-        
-        
-        System.out.println("----------Aeropuertos----------");
-        AeropuertoClass was = new AeropuertoClass();
-        
-        was.leerAeropuertoTxt();
-        
-        System.out.println(was.getID() + "\n\n" + was.getNOMBRE() + "\n\n" + was.getPAISES());
-        
-        
-        System.out.println("----------Tripulaciones----------");
-        TripulacionesClass nene = new TripulacionesClass();
-        
-        nene.LeerTripulacionesTxt();
-        
-        System.out.println(nene.getCEDULA()+ "\n\n" + nene.getNOMBRES() + "\n\n" + nene.getIDS()
-                + "\n\n" + nene.getROL() + "\n\n" + nene.getESTADO());
-        
-        System.out.println("----------Aviones----------");
-        AvionesClass sese = new AvionesClass();
-        
-        sese.LeerAvionesTxt();
-        
-        System.out.println(sese.getIDS() + "\n\n" + sese.getMODELO() + "\n\n" + sese.getIDS_AERO() 
-                + "\n\n" + sese.getESTADO());
     }
     
 }
